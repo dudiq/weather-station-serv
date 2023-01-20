fba79713a0e45b18b38a8f4bf352823609985b7d
T118\user <tmateb118@gmail.com>
2023-01-21 00:21:15 +0300

    fix: update fonts, remove unused

--- apps/firmware/fonts/create-font.bat
+++ apps/firmware/fonts/create-font.bat
@@ -1,5 +1,5 @@
 #python fontconvert.py --compress meteocons96 96 demo.ttc > meteocons96.h
 python fontconvert.py --compress textSm 8 inbox/OpenSans-Regular.ttc > dist/textSm.h
-python fontconvert.py --compress textMd 16 inbox/SourceCodePro-Regular.ttc > dist/textMd.h
-python fontconvert.py --compress textLg 32 inbox/SourceCodePro-Regular.ttc > dist/textLg.h
-python fontconvert.py --compress textXl 64 inbox/SourceCodePro-Regular.ttc > dist/textXl.h
+python fontconvert.py --compress textMd 16 inbox/RobotoSlab-Regular.ttc > dist/textMd.h
+python fontconvert.py --compress textLg 28 inbox/RobotoSlab-Regular.ttc > dist/textLg.h
+python fontconvert.py --compress textXl 52 inbox/RobotoSlab-Regular.ttc > dist/textXl.h
